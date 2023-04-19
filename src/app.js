73762484d49e8c1765fbcdda6c970ff4876d0f56
Bianca Dias <bianca.dias@ufv.br>
2023-04-19 08:37:05 -0300

    validações de cadastro concluidas

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { MongoClient } from "mongodb";
 import cors from "cors";
 import joi from 'joi';
 import dotenv from "dotenv";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.listen(5000, console.log("servidor iniciado na porta 5000"))
@@ -18,3 +19,30 @@ try {
     console.log(err.message);
 }
 const db = mongoCilent.db()
+
+const usuarioSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required().min(3)
+})
+
+
+app.post("/cadastro", async (req, res) => {
+    const { name, email, password } = req.body;
+    const validation = usuarioSchema.validate(req.body, { abortEarly: false })
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.path);
+        return res.status(422).send(errors);
+    }
+    const hash = bcrypt.hashSync(password, 10);
+    try{
+        const verificaSePossuiCadastro = await db.collection("cadastrados").findOne({email})
+        if(verificaSePossuiCadastro){
+            return res.status(409).send("Usuario já cadastrado")
+        }
+        await db.collection("cadastrados").insertOne({name, email, password: hash})
+        res.sendStatus(201)
+    }catch(err){
+        res.sendStatus(500);
+    }
+})
