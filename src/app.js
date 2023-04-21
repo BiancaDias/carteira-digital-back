9de13ffd005536417bd1f5a3905a54daa967f2d6
Bianca Dias <bianca.dias@ufv.br>
2023-04-21 12:06:45 -0300

    exibição de transações concluido

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import joi from 'joi';
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import dayjs from "dayjs";
 
 const app = express();
 app.listen(5000, console.log("servidor iniciado na porta 5000"))
@@ -65,10 +66,70 @@ app.post("/", async (req, res) => {
         const verificaSenha = bcrypt.compareSync(password, verificaSePossuiCadastro.password);
         if(!verificaSenha) return res.status(401).send("senha incorreta")
         const token = uuid();
-        await db.collection("logados").insertOne({token, idUsuario: verificaSePossuiCadastro._id})
+        await db.collection("logados").insertOne({token, name:verificaSePossuiCadastro.name, email: verificaSePossuiCadastro.email})
         res.status(200).send({token, name:verificaSePossuiCadastro.name})
     }catch(err){
         res.status(500).send(err)
     }
 
+})
+
+app.post("/logout", async (req, res) => {
+    const { token } = req.body;
+    console.log(token)
+    try{
+        const del = await db.collection("logados").deleteOne({token});
+        if(del.deletedCount===0) return res.sendStatus(400)
+        res.sendStatus(200);
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.post("/transaction", async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) return res.sendStatus(401);
+
+    const transactionSchema = joi.object({
+        valor: joi.number().positive().precision(2).required(),
+        descricao: joi.string().required(),
+        tipo: joi.string().valid("saida","entrada").required()
+    })
+
+    const dataAtual = dayjs().format('DD/MM');
+    const validation = transactionSchema.validate(req.body, { abortEarly: false })
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422).send(errors)
+    }
+    const {valor, descricao, tipo} = req.body;
+    try{
+        const user = await db.collection("logados").findOne({token});
+        if(!user) return res.sendStatus(404)
+        
+        await db.collection("transactions").insertOne({user: user.email, type: tipo, dia: dataAtual, valor, descricao})
+        
+        res.sendStatus(201)
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.get("/transaction", async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) return res.sendStatus(401);
+
+    try{
+        const userFind = await db.collection("logados").findOne({token});
+        if(!userFind) return res.sendStatus(404)
+        const transactions = await db.collection("transactions").find({user: userFind.email}).toArray();
+        res.send(transactions.reverse())
+    }catch(err){
+        res.status(500).send(err);
+    }
 })
